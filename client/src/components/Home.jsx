7e21bcf535d303e2e7f8a54d96e27134bfde3018
Zulqarnain Chohan <zulqarnainc67@gmail.com>
2024-01-06 10:51:23 -0800

    fix css problems

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
     <>
       <div className="home-container">
          <div className="home-content-container">
-            <h1>Get your <span className='blue'>Dream Job</span> Today</h1>
+            <h1>Get  Your  <span className='blue'>Dream Job</span> Today!</h1>
             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos excepturi facilis nemo facere ducimus, laborum suscipit eligendi? Totam amet dolore ipsum doloribus explict suscipit dolores nesciunt illum doloribus placeat fuga nihil laborum het yo uk kjasd lfsjio labore consectetur eius beatae.</p>
             <button className="apply">Apply</button>
           </div>  
